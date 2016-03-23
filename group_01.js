a1f69d79f29bc5557f7992f6bf0ff4881f19aa06
Ryan Mulcahy <ryan@primeacademy.io>
2016-03-23 09:09:26 -0500

    Cleanup

--- group_01.js
+++ group_01.js
@@ -1,14 +1,14 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
+var atticus = ['Atticus', '2405', '47000', 3];
+var jem = ['Jem', '62347', '63500', 4];
+var boo = ['Boo', '11435', '54000', 3];
+var scout = ['Scout', '6243', '74750', 5];
 
 var employees = [atticus, jem, boo, scout];
 
-var summaries = [];
 for (var i = 0; i < employees.length; i++) {
-   console.log(employeeToSummary(employees[i]));
+  console.log(employeeToSummary(employees[i]));
 }
+
 /*
 - The first item should also contain the employees name.
 - The second item should contain the percentage of STI the employee is to receive.
@@ -34,9 +34,7 @@ and should receive an additional 5%.
 - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
 - No bonus can be above 13% total.
 */
-
 function calcSTIPercentage(employee) {
-  var name = employee[0];
   var employeeNumber = employee[1];
   var salary = parseInt(employee[2]);
   var rating = employee[3];
@@ -45,17 +43,17 @@ function calcSTIPercentage(employee) {
 
   if (rating > 2) {
     switch (rating) {
-      case 3:
-        percentage = .04;
-        break;
-      case 4:
-        percentage = .06;
-        break;
-      case 5:
-        percentage = .10;
-        break;
-      default:
-        percentage = 0;
+    case 3:
+      percentage = .04;
+      break;
+    case 4:
+      percentage = .06;
+      break;
+    case 5:
+      percentage = .10;
+      break;
+    default:
+      percentage = 0;
     }
 
     if (employeeNumber.length <= 4) {
